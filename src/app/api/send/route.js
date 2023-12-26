4cabcb50e0e65880a27dafaa2402543d652ef265
sahasuvankar7 <suvankarsaha084@gmail.com>
2023-12-27 02:08:06 +0530

    updated some files

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,53 +1,55 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-import nodemailer from 'nodemailer';
 
-// export async function GET() {
-//   const resend = new Resend(process.env.RESEND_API_KEY);
-//   try {
-//     const data = await resend.emails.send({
-//       from: 'suvankarsaha701@gmail.com',
-//       to: 'suvankarsaha084@gmail.com',
-//       subject: "chok gayyeeee",
-//       html:'<p>system fadd dengeeee</p>'
-//     });
-
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     console.error('error sending email:',error);
-//     return NextResponse.json({ error: " failed to send email" });
-//   }
-// }
-
-
+// const resend = new Resend(process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = req;
+  const { body } = await req.json();
   const { email, subject, message } = body;
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: ['suvankarsaha084@gmail.com', email],
+      from: email,
+      to: [fromEmail],
       subject: subject,
       react: (
         <>
-          <h1>{subject}</h1>
+          <h1>{body.subject}</h1>
           <p>Thank you for contacting with us</p>
-          <p>{message}</p>
+          <p>{body.message}</p>
         </>
       ),
     });
 
-    console.log(NextResponse.json(data));
+    console.log("email send response", data);
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return NextResponse.json({
+      error: "failed to send the error",
+      details: error.message,
+    });
   }
 }
 
-// export async function POST( req){
-//   const{email,subject, message} = await req.json();
-//   const transporter = 
+// export async function POST() {
+//   const resend = new Resend(process.env.RESEND_API_KEY);
+//   try {
+//     const {data,email} = await resend.emails.send({
+//       from: 'suvankarsaha701@gmail.com',
+//       to: 'suvankarsaha037@gmail.com',
+//       subject: "chok gayyeeee",
+//       html:'<p>system fadd dengeeee</p>'
+//     });
+
+  
+//     return NextResponse.json({
+//       status:"ok",
+//     });
+//   } catch (error) {
+//     console.error('error sending email:',error);
+//     return NextResponse.json({ error: " failed to send email" });
+//   }
 // }
